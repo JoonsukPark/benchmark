a1cf9560fd44afd8cb43b443be48f7c0ba85c60c
Joonsuk Park <park.1952@osu.edu>
2018-05-06 22:34:20 -0400

    revision

--- benchmark_logit.py
+++ benchmark_logit.py
@@ -1,3 +1,8 @@
+## Benchmark testing, pyspark.ml.classification vs sklearn.linear_model
+## Target classification algorithm: Binary logistic regression
+## Use the MNIST dataset for demonstration
+## Label: Whether the digit is 5
+
 import sklearn
 from sklearn.datasets import fetch_mldata
 from pyspark.sql import SparkSession
@@ -7,16 +12,26 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import LogisticRegression
 import time
 
+# import dataset
+
 mnist = fetch_mldata('MNIST original')
+mnist_X = mnist['data']
+
+# target: if the digit is 5
+
+mnist_y = mnist['target']==5
+mnist_data = np.c_[mnist_X, mnist_y]
+
+## 1. PySpark
+# Create a SparkSession
 
 spark = SparkSession.builder \
      .master("local") \
      .appName("Logistic Regression") \
      .getOrCreate()
 
-mnist_X = mnist['data']
-mnist_y = mnist['target']==5
-mnist_data = np.c_[mnist_X, mnist_y]
+# data preprocessing
+
 ncols = mnist_data.shape[1]
 colnames = []
 for i in np.arange(1,ncols, 1):
@@ -30,19 +45,24 @@ schema[-1] = StructField('label', IntegerType(), True)
 schema = StructType(schema)
 mnist_data = mnist_data.tolist()
 mnist_dataframe = spark.createDataFrame(mnist_data, schema)
-colnames.pop()
 
+colnames.pop()
 assembler = VectorAssembler(
     inputCols=colnames,
     outputCol="features")
 
 mnist_dataframe = assembler.transform(mnist_dataframe)
+
+# Fit the model and measure the execution time
+
 lr_pyspark = LogisticRegression()
 
 start = time.time()
 lrPySpark = lr_pyspark.fit(mnist_dataframe)
 end = time.time()
 print("Model fitting using the pyspark.ml.classification module took {} seconds.".format(round(end-start,2)))
+
+## 2. Do the same thing using scikit-learn
 
 from sklearn.linear_model import LogisticRegression
 
